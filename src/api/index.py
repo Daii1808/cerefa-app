8494f96c67419dcd2cfeb14c14caa784afd2406c
Daniel Matus <lilshyni2036@gmail.com>
2026-05-18 22:18:37 -0400

    prueba de error vercel

--- src/api/index.py
+++ src/api/index.py
@@ -1,5 +1,11 @@
+import os
+import sys
+
+# El truco maestro va en la línea 1 y 2 para que arregle el mapa ANTES de importar nada
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 from flask import Flask, jsonify
-from src.config.config import Config  # ➔ Apunta a tu carpeta config real
+from src.config.config import Config
 from src.database.models import db
 from src.api.routes.fauna import fauna_bp
 
@@ -15,13 +21,13 @@ app.register_blueprint(fauna_bp)
 
 @app.route('/')
 def home():
-    return "¡Backend CEREFA con Arquitectura Modular de Pizarra corriendo en Vercel!"
+    return "¡Backend CEREFA corriendo impecable!"
 
 @app.route('/api/test')
 def test():
     return jsonify({
         "status": "online",
-        "arquitectura": "N-Capas Estructurada (Real)"
+        "arquitectura": "N-Capas Estructurada"
     })
 
 if __name__ == '__main__':
