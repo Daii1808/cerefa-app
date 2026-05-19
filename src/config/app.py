f0041069f9338752593251397371119766b7cade
Daniel Matus <lilshyni2036@gmail.com>
2026-05-19 01:36:02 -0400

    Actualizando estructura CEREFA

---
+++ src/config/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_KEY")
+)
+
+@app.route('/')
+def index():
+    response = supabase.table('todos').select("*").execute()
+    todos = response.data
+
+    html = '<h1>Todos</h1><ul>'
+    for todo in todos:
+        html += f'<li>{todo["name"]}</li>'
+    html += '</ul>'
+    return html
+
+application = app
+
+if __name__ == '__main__':
+    app.run(debug=True)
